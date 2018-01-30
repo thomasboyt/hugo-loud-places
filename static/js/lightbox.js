22050b6a284d5a5bac6837b9de60c6724c7e99d9
Thomas Boyt <me@thomasboyt.com>
2018-01-30 17:31:02 -0500

    Remove leftover console.log

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('dom content loaded')
   const imageLinks = document.querySelectorAll('.flexbin a');
 
   for (let imageLink of imageLinks) {
